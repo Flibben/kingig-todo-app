e550e2443c27cb4c335ca1573364f701ee912ec7
Flibben <flibben@gmail.com>
2021-09-28 13:37:04 +0200

    added mockup data

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,27 @@
 import React from "react";
 import "./App.css";
 
+const data = [
+  {
+    id: 1,
+    title: "Fett skön titel",
+    body: "this is where the niceseness begins!",
+    lastModified: "2021-09-24"
+  },
+  {
+    id: 2,
+    title: "Gymövningar",
+    body: "biceps, triceps, chest",
+    lastModified: "2021-09-23"
+  },
+  {
+    id: 3,
+    title: "Gainfeed",
+    body: "only aminoacids for my body",
+    lastModified: "2021-09-21"
+  },
+]
+
 const App = () => {
   return <div>test</div>;
 };
