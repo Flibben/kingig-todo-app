f50dd79ab434a058307294f94b2b22b8333196d0
Axel Thiel <axth2220@gmail.com>
2021-09-29 14:51:15 +0200

    move todo list to context

--- client/src/components/CardList/CardList.jsx
+++ client/src/components/CardList/CardList.jsx
@@ -1,34 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Grid } from '@mui/material';
 import { TodoCard } from "../TodoCard/TodoCard";
-
-const data = [
-  {
-    id: 1,
-    title: "Fett skön titel",
-    body: "Work on the sköna in the life",
-    lastModified: "2021-09-24",
-  },
-  {
-    id: 2,
-    title: "Gymövningar",
-    body: "biceps, triceps, chest",
-    lastModified: "2021-09-23",
-  },
-  {
-    id: 3,
-    title: "Gainfeed",
-    body: "only aminoacids for my body",
-    lastModified: "2021-09-21",
-  },
-];
-
+import { TodoContext } from "../../context/TodoProvider";
 
 export const CardList = () => {
-
+  const { todoList } = useContext(TodoContext);
   return (
     <Grid container spacing={2}>
-      {data.map(({ id, title, body, lastModified }) => {
+      {todoList.map(({ id, title, body, lastModified }) => {
         return (
           <TodoCard key={id} title={title} body={body} lastModified={lastModified} />
         );
--- client/src/context/TodoProvider.jsx
+++ client/src/context/TodoProvider.jsx
@@ -3,7 +3,26 @@ import React, { useState, createContext } from 'react'
 export const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
-  const [todoList, setTodoList] = useState(['test']);
+  const [todoList, setTodoList] = useState([
+    {
+      id: 1,
+      title: "Fett skön titel",
+      body: "Work on the sköna in the life",
+      lastModified: "2021-09-24",
+    },
+    {
+      id: 2,
+      title: "Gymövningar",
+      body: "biceps, triceps, chest",
+      lastModified: "2021-09-23",
+    },
+    {
+      id: 3,
+      title: "Gainfeed",
+      body: "only aminoacids for my body",
+      lastModified: "2021-09-21",
+    },
+  ]);
 
   return (
     <TodoContext.Provider value={{ todoList, setTodoList }}>
