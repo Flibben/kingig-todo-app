60f41f9cb0cfa56df0900e7e460765b926681162
Axel Thiel <axth2220@gmail.com>
2021-09-30 16:40:54 +0200

    created DB connection

--- server/app.js
+++ server/app.js
@@ -1,27 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const cors = require('cors');
-const todoRoutes = require('./routes/todoRoute')
+const todoRoutes = require('./routes/todoRoute');
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 
 app.use("/todos", todoRoutes);
 
-const CONNECTION_URL = "";
+const CONNECTION_URL = `mongodb+srv://admin:${process.env.PASSWORD}@todocluster.hppc4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 const PORT = process.env.PORT || 5000;
 
-// mongoose
-//   .connect(CONNECTION_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() =>
-//     app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))
-//   )
-//   .catch((error) => console.log(error.message));
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
 
 // mongoose.set("useFindAndModify", false);
 
-app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))
+// app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`))
