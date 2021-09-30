37dc00b1ae6346e5441f54757f1b60b4723364cd
Axel Thiel <axth2220@gmail.com>
2021-09-30 15:31:44 +0200

    fix mispelled route

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose")
 const cors = require('cors');
 const todoRoutes = require('./routes/todoRoute')
+console.log(todoRoutes)
 
 
 const app = express();
--- server/routes/todoRoute.js
+++ server/routes/todoRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const { model } = require('mongoose');
 const getAllTodos = require('../controllers/todoController')
+console.log(getAllTodos)
 
 const router = express.Router();
 
 router.get('/', getAllTodos)
 
-module.exports = {
-    
-}
+module.exports = router;
