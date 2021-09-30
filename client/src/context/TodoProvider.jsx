ee13543fe6fc945e53633ca6885ab6ba306c48d8
Axel Thiel <axth2220@gmail.com>
2021-09-30 16:10:31 +0200

    set todolist initial state to null

--- client/src/context/TodoProvider.jsx
+++ client/src/context/TodoProvider.jsx
@@ -3,26 +3,7 @@ import React, { useState, createContext } from 'react'
 export const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
-  const [todoList, setTodoList] = useState([
-    {
-      id: 1,
-      title: "Fett skön titel",
-      body: "Work on the sköna in the life",
-      lastModified: "2021-09-24",
-    },
-    {
-      id: 2,
-      title: "Gymövningar",
-      body: "biceps, triceps, chest",
-      lastModified: "2021-09-23",
-    },
-    {
-      id: 3,
-      title: "Gainfeed",
-      body: "only aminoacids for my body",
-      lastModified: "2021-09-21",
-    },
-  ]);
+  const [todoList, setTodoList] = useState(null);
 
   return (
     <TodoContext.Provider value={{ todoList, setTodoList }}>
