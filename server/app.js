913fb0a22bfe25bf98f1cbcab62e4031af6bbba9
Axel Thiel <axth2220@gmail.com>
2021-09-30 16:10:48 +0200

    remove unneccessary logs

--- server/app.js
+++ server/app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose")
 const cors = require('cors');
 const todoRoutes = require('./routes/todoRoute')
-console.log(todoRoutes)
-
 
 const app = express();
 app.use(cors());
--- server/routes/todoRoute.js
+++ server/routes/todoRoute.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { model } = require('mongoose');
 const getAllTodos = require('../controllers/todoController')
-console.log(getAllTodos)
 
 const router = express.Router();
 
