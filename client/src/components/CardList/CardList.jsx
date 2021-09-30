8ab9b758b7119825154cfce84039e8af3bbe17a4
Axel Thiel <axth2220@gmail.com>
2021-09-30 16:09:56 +0200

    add useeffect to fetch todos

--- client/src/components/CardList/CardList.jsx
+++ client/src/components/CardList/CardList.jsx
@@ -1,16 +1,24 @@
-import React, { useContext } from "react";
-import { Grid } from '@mui/material';
+import React, { useContext, useEffect } from "react";
+import { CircularProgress, Grid } from '@mui/material';
 import { TodoCard } from "../TodoCard/TodoCard";
 import { TodoContext } from "../../context/TodoProvider";
 
 export const CardList = () => {
-  const { todoList } = useContext(TodoContext);
+
+  const { todoList, setTodoList } = useContext(TodoContext);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/todos').then(response => response.json()).then(data => setTodoList(data))
+  }, [setTodoList])
+
   return (
     <Grid container spacing={2}>
-      {todoList.map(({ id, title, body, lastModified }) => {
-        return (
-          <TodoCard key={id} id={id} title={title} body={body} lastModified={lastModified} />
-        );
-      })}
+      {
+        !todoList ? <CircularProgress /> :
+          todoList.map(({ id, title, body, lastModified }) => {
+            return (
+              <TodoCard key={id} id={id} title={title} body={body} lastModified={lastModified} />
+            );
+          })}
     </Grid>)
 };
