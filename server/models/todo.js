384be6e2e67ac7816ee84104607395588b99075d
Flibben <flibben@gmail.com>
2021-10-05 14:32:40 +0200

    required fields and added userRef

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 
 const todoSchema = mongoose.Schema({
-    title: {type: String, required: true},
+    title: { type: String, required: true },
     body: String,
-    date: Date
+    date: Date,
+    userRef: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' }
 })
 
 const TodoModel = mongoose.model('Todo', todoSchema);
