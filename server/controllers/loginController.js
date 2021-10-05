d22dcdc95af4a15dd156d4df61b08983ab684a21
Axel Thiel <axth2220@gmail.com>
2021-10-05 16:42:12 +0200

    add login controller

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,17 +1,26 @@
-const { findOne } = require('../models/user');
+const bcrypt = require('bcrypt')
 const UserModel = require('../models/user')
 
 const login = async (req, res) => {
     const {password, email} = req.body;
 
     try {
-      const user = findOne({email});
-    
-    } catch (error) {
-      
+      const user = await UserModel.findOne({email});
+      if(!user){
+        res.json({message: 'That email doesn\'t exist'})
+        return;
+      }
+      bcrypt.compare(password, user.password).then((result) => {
+        if(result){
+          return res.send('LOGGED IN')
+        }
+        return res.send('Wrong password, try something else!')
+    });
     }
-    res.send('fett äkta login')
+    catch (error) {
+      res.status(500).json({message: error.message})
+    }
+
 }
 
-
-  
+module.exports = login;
