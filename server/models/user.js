3af730499ec429ebc4e4b35f483f215e8f315d56
Flibben <flibben@gmail.com>
2021-10-05 14:32:09 +0200

    created user model

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  fullName: { type: String, required: true },
+  displayName: { type: String, required: true },
+  password: { type: String, required: true },
+  mail: { type: String, required: true, unique: true }
+})
+
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
