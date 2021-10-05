e01ff9d69ad27acfd499fbbccf13ab2101fc2620
Flibben <flibben@gmail.com>
2021-10-05 13:00:35 +0200

    imported route

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose")
 const cors = require('cors');
 const todoRoutes = require('./routes/todoRoute');
+const authRoutes = require('./routes/authRoute')
 require("dotenv").config();
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json())
 
 app.use("/todos", todoRoutes);
+app.use("/auth", authRoutes);
 
 const CONNECTION_URL = `mongodb+srv://admin:${process.env.PASSWORD}@todocluster.hppc4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
