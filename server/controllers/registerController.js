adac62dcd384bab6407fad9bc50958196e277119
Flibben <flibben@gmail.com>
2021-10-06 15:40:46 +0200

    Add missing return statement

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -21,19 +21,21 @@ const register = async (req, res) => {
     res.status(400).send('Is that REALLY a valid eemail?')
     return
   }
-  const dbeemail = await UserModel.findOne({email});
-  if(dbeemail){
+  const dbeemail = await UserModel.findOne({ email });
+  if (dbeemail) {
     res.status(500).send('A user with that eemail already exists.')
+    return
   }
 
   bcrypt.hash(password, 10).then(async (hash) => {
-    const user = new UserModel({fullName, displayName, password: hash, email})
-    try {    
+    const user = new UserModel({ fullName, displayName, password: hash, email })
+    try {
       await user.save();
       res.status(200).send('Success!');
     } catch (error) {
-      res.status(500).json({message: error.message});
+      res.status(500).json({ message: error.message });
     }
+
   })
 
 }
