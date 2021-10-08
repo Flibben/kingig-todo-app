ff7294bf3ecb663996fbe63f9a0fb4c144c641bb
Flibben <flibben@gmail.com>
2021-10-08 12:27:55 +0200

    useState for loggedin/or not

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Redirect } from "react-router-dom";
 import { CardList } from "./components/CardList/CardList";
 import Container from "@mui/material/Container";
 import { InputNewTodo } from "./components/InputNewTodo/InputNewTodo";
 import { TodoProvider } from "./context/TodoProvider";
 import "./App.css";
+import { Login } from "./components/Login/Login";
 
 const App = () => {
+  const [token, setToken] = useState();
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <TodoProvider>
       <Container>
