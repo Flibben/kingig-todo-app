596e3a9a5c3eb8a30b4797bf86d1d0f378381811
Axel Thiel <axth2220@gmail.com>
2021-10-08 14:26:54 +0200

    add token state to todoprovider

--- client/src/context/TodoProvider.jsx
+++ client/src/context/TodoProvider.jsx
@@ -3,10 +3,11 @@ import React, { useState, createContext } from 'react'
 export const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
+  const [token, setToken] = useState(null)
   const [todoList, setTodoList] = useState(null);
 
   return (
-    <TodoContext.Provider value={{ todoList, setTodoList }}>
+    <TodoContext.Provider value={{ todoList, setTodoList, token, setToken }}>
       {children}
     </TodoContext.Provider>
   )
