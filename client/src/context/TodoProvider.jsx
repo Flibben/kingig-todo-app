d58bbba49b74c3007497c75673f4b112c8c38a76
Axel Thiel <axth2220@gmail.com>
2021-10-09 09:35:23 +0200

    add useToken hook

--- client/src/context/TodoProvider.jsx
+++ client/src/context/TodoProvider.jsx
@@ -1,9 +1,10 @@
 import React, { useState, createContext } from 'react'
+import { useToken } from '../hooks/useToken';
 
 export const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
-  const [token, setToken] = useState(null)
+  const { token, setToken } = useToken();
   const [todoList, setTodoList] = useState(null);
 
   return (
