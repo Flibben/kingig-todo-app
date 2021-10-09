439e6b6dcfcaf304c1e785316663f3d65b49c375
Flibben <flibben@gmail.com>
2021-10-09 12:30:15 +0200

    indentation

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,27 +1,26 @@
 const TodoModel = require('../models/todo')
 
-  const getAllTodos = async (req, res) => {
-     const todos = await TodoModel.find();
-      res.json(todos);
-  }
+const getAllTodos = async (req, res) => {
+  const todos = await TodoModel.find();
+  res.json(todos);
+}
 
-  const createNewTodo = async(req, res) => {
-    const newTodo = new TodoModel(req.body); 
-    const response = await newTodo.save();
-    res.json(response);
-  }
+const createNewTodo = async (req, res) => {
+  const newTodo = new TodoModel(req.body);
+  const response = await newTodo.save();
+  res.json(response);
+}
 
-  const deleteTodo = async(req, res) => {
-    const todoID = req.params.id;
-    await TodoModel.deleteOne({_id: todoID});
-    res.status(200).send();
-  }
+const deleteTodo = async (req, res) => {
+  const todoID = req.params.id;
+  await TodoModel.deleteOne({ _id: todoID });
+  res.status(200).send();
+}
 
-  const editTodo = async(req, res) => {
-    const todoID = req.body._id;
-    const response = await TodoModel.findByIdAndUpdate({_id:todoID}, req.body, {new: true});
-    console.log(response);
-    res.json(response);
-  }
+const editTodo = async (req, res) => {
+  const todoID = req.body._id;
+  const response = await TodoModel.findByIdAndUpdate({ _id: todoID }, req.body, { new: true });
+  res.json(response);
+}
 
-  module.exports = {getAllTodos, createNewTodo, deleteTodo, editTodo};
+module.exports = { getAllTodos, createNewTodo, deleteTodo, editTodo };
