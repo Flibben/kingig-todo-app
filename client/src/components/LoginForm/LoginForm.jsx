0c78a851eef88d15a613f34f9d8db2f14ea4e156
Flibben <flibben@gmail.com>
2021-10-09 12:32:15 +0200

    Added: UseEffect, Handlechange, handlesubmit

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,11 +1,43 @@
-import { TextField, Box } from '@mui/material'
-import React from 'react'
+import { TextField, Box, Button } from '@mui/material'
+import React, { useState, useContext, useEffect } from 'react'
+import { TodoContext } from '../../context/TodoProvider'
+import { useHistory } from 'react-router'
 
 export const LoginForm = () => {
+  const { token, setToken } = useContext(TodoContext)
+  const [loginValue, setLoginValue] = useState({ email: "", password: "" })
+  const history = useHistory()
+
+  useEffect(() => {
+    if (token) {
+      history.push('/todos')
+    }
+  }, [token])
+
+  const handleChange = (e) => {
+    setLoginValue({ ...loginValue, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch('http://localhost:5000/login', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(loginValue)
+    }).then(res => res.json()).then(data => {
+      setToken(data)
+    })
+  }
+
+
   return (
-    <Box component="form">
-      <TextField id="email" label="email" type="email" />
-      <TextField id="password" label="Password" type="password" />
+    <Box component="form" onChange={handleChange} onSubmit={handleSubmit}>
+      <TextField name="email" label="email" type="email" />
+      <TextField name="password" label="Password" type="password" />
+      <Button type="submit">Login</Button>
+      <a href="/todos">hejhopp</a>
     </Box>
   )
 }
