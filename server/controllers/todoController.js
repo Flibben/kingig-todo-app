5d19614f26a33bb0c6ab0a25b4d0616a4a310017
Flibben <flibben@gmail.com>
2021-10-12 20:25:43 +0200

    Remove logs. Add try/catch for errorhandling

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -2,29 +2,41 @@ const TodoModel = require('../models/todo')
 
 const getAllTodos = async (req, res) => {
   const userRef = req.body.userRef;
-  console.log(userRef);
   const todos = await TodoModel.find({}).where({ userRef });
-  console.log(todos);
   res.json(todos);
 }
 
 const createNewTodo = async (req, res) => {
-  const newTodo = new TodoModel(req.body);
-  const response = await newTodo.save();
-  res.json(response);
+  try {
+    const newTodo = new TodoModel(req.body);
+    const response = await newTodo.save();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(403).json(error)
+  }
 }
 
 const deleteTodo = async (req, res) => {
-  const todoID = req.params.id;
-  const userRef = req.body.userRef;
-  await TodoModel.deleteOne({ _id: todoID }).where(userRef);
-  res.status(200).send();
+  try {
+    const todoID = req.params.id;
+    const userRef = req.body.userRef;
+    await TodoModel.deleteOne({ _id: todoID }).where(userRef);
+    res.status(200).json();
+
+  } catch (error) {
+    res.status(403).json(error)
+  }
 }
 
 const editTodo = async (req, res) => {
-  const todoID = req.body._id;
-  const response = await TodoModel.findByIdAndUpdate({ _id: todoID }, req.body, { new: true });
-  res.json(response);
+  try {
+    const todoID = req.body._id;
+    const userRef = req.body.userRef;
+    const response = await TodoModel.findByIdAndUpdate({ _id: todoID }, req.body, { new: true }).where(userRef);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(403).json(error)
+  }
 }
 
 module.exports = { getAllTodos, createNewTodo, deleteTodo, editTodo };
