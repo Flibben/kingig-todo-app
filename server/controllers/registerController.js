068a2404cf4e9b0068604c9416de1a46e1a62ab1
Axel Thiel <axth2220@gmail.com>
2021-10-26 18:14:07 +0200

    fix error handling be

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -6,26 +6,30 @@ const register = async (req, res) => {
     fullName, displayName, password, email,
   } = req.body;
 
-  if (password.length < 6) {
-    res.status(403).json({ message: 'Password must be at least 6 characters long.' });
-    return;
-  }
-  if (fullName.length > 50) {
-    res.status(403).json({ message: 'Full name can\'t be longer than 50 characters.' });
-    return;
-  }
-  if (displayName.length < 1) {
-    res.status(403).json({ message: 'Display name must be at least 1 character.' });
-    return;
-  }
-  if (email.length < 6) {
-    res.status(403).json({ message: 'Please enter a valid email?' });
-    return;
-  }
-  const dbEmail = await UserModel.findOne({ email });
-  if (dbEmail) {
-    res.status(500).json({ message: 'A user with that email already exists.' });
-    return;
+  try {
+    if (password.length < 6) {
+      res.status(403).json({ message: 'Password must be at least 6 characters long.' });
+      return;
+    }
+    if (fullName.length > 50) {
+      res.status(403).json({ message: 'Full name can\'t be longer than 50 characters.' });
+      return;
+    }
+    if (displayName.length < 1) {
+      res.status(403).json({ message: 'Display name must be at least 1 character.' });
+      return;
+    }
+    if (email.length < 6) {
+      res.status(403).json({ message: 'Please enter a valid email?' });
+      return;
+    }
+    const dbEmail = await UserModel.findOne({ email });
+    if (dbEmail) {
+      res.status(500).json({ message: 'A user with that email already exists.' });
+      return;
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong' })
   }
 
   bcrypt.hash(password, 10).then(async (hash) => {
