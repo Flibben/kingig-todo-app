f4dc0f4221b51bcb7919fceeaaec28c835af23bc
Flibben <flibben@gmail.com>
2021-10-26 14:25:27 +0200

    add logoutbutton

--- client/src/pages/TodoPage.jsx
+++ client/src/pages/TodoPage.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { InputNewTodo } from '../components/InputNewTodo/InputNewTodo';
 import { CardList } from '../components/CardList/CardList';
 import { UserName } from '../components/UserName/UserName';
+import { LogOutButton } from '../components/LogOutButton/LogOutButton';
 
 export const TodoPage = () => (
   <div>
+    <LogOutButton />
     <UserName />
     <InputNewTodo />
     <CardList />
