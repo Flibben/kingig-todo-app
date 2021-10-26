3f01f398953d1680d98932ed0267839b69d20d67
Axel Thiel <axth2220@gmail.com>
2021-10-26 17:42:32 +0200

    change baseurl

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,2 +1,1 @@
-// export const BASE_URL = 'https://todoapp-backend3.herokuapp.com/';
-export const BASE_URL = 'http://localhost:5000/';
+export const BASE_URL = 'https://todoapp-backend3.herokuapp.com/';
