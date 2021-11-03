6245e42519f16ab1af726d64d2c1ba3c09081476
Axel Thiel <axth2220@gmail.com>
2021-11-03 16:10:46 +0100

    disable some eslint rules for testing

--- client/src/__tests__/RegisterForm.test.js
+++ client/src/__tests__/RegisterForm.test.js
@@ -1,23 +1,21 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import {RegisterForm} from "../components/RegisterForm/RegisterForm";
-
+import { RegisterForm } from '../components/RegisterForm/RegisterForm';
 
 test('that the component renders 4 input fields for registration', () => {
   render(<RegisterForm />);
-  const emailInput = screen.getByTestId("email");
+  const emailInput = screen.getByTestId('email');
   expect(emailInput).toBeInTheDocument();
 
-  const passwordInput = screen.getByTestId("password");
+  const passwordInput = screen.getByTestId('password');
   expect(passwordInput).toBeInTheDocument();
-  
-  const fullnameInput = screen.getByTestId("fullname")
+
+  const fullnameInput = screen.getByTestId('fullname');
   expect(fullnameInput).toBeInTheDocument();
 
-  const displayName = screen.getByTestId("displayname")
+  const displayName = screen.getByTestId('displayname');
   expect(displayName).toBeInTheDocument();
 });
-
-
-
